c6054393075fb177331d4c9a5fc08e58c9466ffe
Saurav Kumar <arvsaurav@gmail.com>
2023-09-11 21:06:35 +0530

    made news-content div clickable

--- src/components/NewsShow.js
+++ src/components/NewsShow.js
@@ -8,13 +8,16 @@ function NewsShow({ renderedNews }) {
     const source = renderedNews.source.name;
     const image = renderedNews.urlToImage;
 
+    const handleClick = () => {
+        window.open(redirectToArticle, '_blank', 'noreferrer');
+    };
+
     return (
         <div className='news-container'>
-            <div className='news-content'>
+            <div className='news-content' onClick={ handleClick }>
                 <h2>{ title }</h2>
                 <p>{ description }</p>
                 <p id='source-id'>{ source }</p>
-                <a href={ redirectToArticle } target='_blank' rel='noreferrer'>View full article</a>
             </div>
             <div className='news-image'>
                 <img src={ image } alt={ title } />
